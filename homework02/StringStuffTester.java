03dce7a774fb904699d94e138df664aea1b5c83d
Hayato <hhori@lion.lmu.edu>
2017-02-07 11:58:10 -0800

    Update

--- homework02/StringStuffTester.java
+++ homework02/StringStuffTester.java
@@ -58,6 +58,10 @@ public class StringStuffTester {
       try { System.out.println( StringStuff.isPalindrome("234") ? "true" : "false" ); }
       catch( Exception e ) { System.out.println ( false ); }
 
+      System.out.print( "   Test for isPalindrome: " );
+      try { System.out.println( StringStuff.isPalindrome("") ? "true" : "false" ); }
+      catch( Exception e ) { System.out.println ( false ); }
+
    }
 
   /**
@@ -69,32 +73,49 @@ public class StringStuffTester {
       System.out.print( "   Test for evensOnly(): " );
       try { System.out.println( "RHRL".equals(StringStuff.evensOnly("REHEARSALS")) ? "RHRL" : "does not work" ); }
       catch( Exception e ) { System.out.println ( false ); }
+
+      System.out.print( "   Test for evensOnly(): " );
+      try { System.out.println( "Hll".equals(StringStuff.evensOnly("Hello")) ? "Hll" : "does not work" ); }
+      catch( Exception e ) { System.out.println ( false ); }
    }
 
   /**
    * test method to test out the operation of the oddsOnly method
    */
    public static void test_oddsOnly() {
-      System.out.println( " One TESTS FOR oddsOnly():" );
+      System.out.println( " TWO TESTS FOR oddsOnly():" );
 
       System.out.print( "   Test for oddsOnly(): " );
       try { System.out.println( "yooes".equals(StringStuff.oddsOnly("xylophones")) ? "yooes" : "does not work" ); }
       catch( Exception e ) { System.out.println ( false ); }
+
+      System.out.print( "   Test for oddsOnly(): " );
+      try { System.out.println( "YooES".equals(StringStuff.oddsOnly("XYloPHonES")) ? "YooES" : "does not work" ); }
+      catch( Exception e ) { System.out.println ( false ); }
+
    }
 
    public static void test_reverse() {
-      System.out.println( " One TESTS FOR reverse():" );
+      System.out.println( " TWO TESTS FOR reverse():" );
 
       System.out.print( "   Test for reverse(): " );
       try { System.out.println( "olleH".equals(StringStuff.reverse("Hello")) ? "olleH" : "does not work" ); }
       catch( Exception e ) { System.out.println ( false ); }
+
+      System.out.print( "   Test for reverse(): " );
+      try { System.out.println( "05".equals(StringStuff.reverse("50")) ? "05" : "does not work" ); }
+      catch( Exception e ) { System.out.println ( false ); }
    }
 
    public static void test_oddsOnlyNoDupes() {
-      System.out.println( " One TESTS FOR oddsOnlyNoDupes():" );
+      System.out.println( " TWO TESTS FOR oddsOnlyNoDupes():" );
 
       System.out.print( "   Test for oddsOnlyNoDupes(): " );
       try { System.out.println( "yoes".equals(StringStuff.oddsOnlyNoDupes("xylophones")) ? "yoes" : "does not work" ); }
+      catch( Exception e ) { System.out.println ( false ); }
+
+      System.out.print( "   Test for oddsOnlyNoDupes(): " );
+      try { System.out.println( "ayo".equals(StringStuff.oddsOnlyNoDupes("Hayato")) ? "ayo" : "does not work" ); }
       catch( Exception e ) { System.out.println ( false ); }
    }
 
@@ -102,10 +123,14 @@ public class StringStuffTester {
    * test method to test out the operation of the evensOnlyNoDupes method
    */
    public static void test_evensOnlyNoDupes() {
-      System.out.println( " One TESTS FOR evensOnlyNoDupes():" );
+      System.out.println( " TWO TESTS FOR evensOnlyNoDupes():" );
 
       System.out.print( "   Test for evensOnlyNoDupes(): " );
       try { System.out.println( "RHL".equals(StringStuff.evensOnlyNoDupes("REHEARSALS")) ? "RHL" : "does not work" ); }
+      catch( Exception e ) { System.out.println ( false ); }
+
+      System.out.print( "   Test for evensOnlyNoDupes(): " );
+      try { System.out.println( "r".equals(StringStuff.evensOnlyNoDupes("Soccer")) ? "r" : "does not work" ); }
       catch( Exception e ) { System.out.println ( false ); }
    }
  }
