f7f4ca086d4c05296cbd5012b7c8b5e018d3008b
Hayato <hhori@lion.lmu.edu>
2017-04-04 11:42:43 -0700

    Final

--- homework05/Ball.java
+++ homework05/Ball.java
@@ -3,7 +3,7 @@
  *  Purpose       :  The main program for the Ball class
  *  @see
  *  @author       :  Hayato Hori
- *  Date written  :  2017-03-21
+ *  Date written  :  2017-04-04
  *  Description   :  This class provides a bunch of methods which may be useful for the Ball class
  *                   for Homework 5, part 1.  Includes the following:
   *
@@ -23,16 +23,12 @@ public class Ball {
    double radius;
    double xPosition;
    double yPosition;
-   private final double xTemp;
-   private final double yTemp;
    double xSpeed;
    double ySpeed;
-   private final double xTempSpeed;
-   private final double yTempSpeed;
 
    /**
    *  Constructor
-   *  This makes a ball with weight 1, radius 4.45, and initializes position at 0,0
+   *  This makes a ball with weight 1, radius .37 in feet, and initializes position based on user input
    */
 
    public Ball(double x, double y, double xSpd, double ySpd) {
@@ -40,12 +36,8 @@ public class Ball {
       radius = 0.37;
       xPosition = x;
       yPosition = y;
-      xTemp = x;
-      yTemp = y;
       xSpeed = xSpd;
       ySpeed = ySpd;
-      xTempSpeed = xSpd;
-      yTempSpeed = ySpd;
    }
 
    public double getRadius() {
@@ -72,32 +64,27 @@ public class Ball {
       return yPosition;
    }
 
-   public double getXSpeed(double xVel, double seconds) {
-      xSpeed = (xVel * seconds) * (1 - (0.01 * seconds));
-      return xSpeed;
-   }
-
-   public double getYSpeed(double yVel, double seconds) {
-      ySpeed = (yVel * seconds) * (1 - (0.01 * seconds));
-      return ySpeed;
-   }
-
    public void setPosition(double x, double y) {
       xPosition = x;
       yPosition = y;
    }
 
+   /**
+   *  Method
+   *  Moves ball according to timeslice
+   */
    public void moveBall(double seconds) {
       if ((Math.sqrt(xSpeed * xSpeed + ySpeed * ySpeed) > 0.083)) {
 
-         while (seconds >= 1.0) {
+         while (seconds >= 1) {
            xPosition += xSpeed;
            yPosition += ySpeed;
-           xSpeed -= 0.01 * xSpeed;
-           ySpeed -= 0.01 * ySpeed;
-           seconds -= 1.0;
+           xSpeed = xSpeed * 0.99;
+           ySpeed = ySpeed * 0.99;
+           seconds -= 1;
          }
-         if (seconds > 0.0) {
+
+         if (seconds > 0) {
            xPosition += xSpeed * seconds;
            yPosition += ySpeed * seconds;
            xSpeed -= 0.01 * xSpeed * seconds;
@@ -106,6 +93,10 @@ public class Ball {
       }
    }
 
+   /**
+   *  Method
+   *  Checks to see if the velocity is low enough
+   */
    public boolean isZeroVelocity() {
       if (xSpeed <= 0.083 && ySpeed <= 0.083) {
          return true;
@@ -114,11 +105,19 @@ public class Ball {
       return false;
    }
 
+   /**
+   *  Method
+   *  Determines distance between two balls
+   */
    public double getDistance(Ball b) {
       double distance = Math.sqrt(Math.pow(b.xPosition - xPosition, 2) + Math.pow(b.yPosition - yPosition, 2));
       return distance;
    }
 
+   /**
+   *  Method
+   *  Checks to see whether or not two balls are colliding
+   */
    public boolean isColliding(Ball b) {
       if (getDistance(b) <= 0.74) {
          return true;
@@ -144,13 +143,30 @@ public class Ball {
       b.moveBall(5);
       System.out.println("The new xPosition of the ball is " + b.getXPosition());
       System.out.println("The new yPosition of the ball is " + b.getYPosition());
-      System.out.println("Getting Speed of Ball at X " + b.getXSpeed(1, 5));
-      System.out.println("Getting Speed of Ball at Y " + b.getYSpeed(2, 5));
-      b.moveBall(10);
+      b.moveBall(2);
       System.out.println("The new xPosition of the ball is " + b.getXPosition());
       System.out.println("The new yPosition of the ball is " + b.getYPosition());
-      System.out.println("Getting Speed of Ball at X " + b.getXSpeed(1, 10));
-      System.out.println("Getting Speed of Ball at Y " + b.getYSpeed(2, 10));
+      b.moveBall(-3);
+      System.out.println("The new xPosition of the ball is " + b.getXPosition());
+      System.out.println("The new yPosition of the ball is " + b.getYPosition());
+      b.moveBall(1);
+      System.out.println("The new xPosition of the ball is " + b.getXPosition());
+      System.out.println("The new yPosition of the ball is " + b.getYPosition());
+      b.moveBall(4);
+      System.out.println("The new xPosition of the ball is " + b.getXPosition());
+      System.out.println("The new yPosition of the ball is " + b.getYPosition());
+      b.moveBall(2);
+      System.out.println("The new xPosition of the ball is " + b.getXPosition());
+      System.out.println("The new yPosition of the ball is " + b.getYPosition());
+      b.moveBall(1);
+      System.out.println("The new xPosition of the ball is " + b.getXPosition());
+      System.out.println("The new yPosition of the ball is " + b.getYPosition());
+      b.moveBall(0);
+      System.out.println("The new xPosition of the ball is " + b.getXPosition());
+      System.out.println("The new yPosition of the ball is " + b.getYPosition());
+      b.moveBall(-1);
+      System.out.println("The new xPosition of the ball is " + b.getXPosition());
+      System.out.println("The new yPosition of the ball is " + b.getYPosition());
       Ball c = new Ball(3, 3, 4, 1);
       System.out.println("The distance between the two balls " + b.getDistance(c));
       System.out.println("Are the balls colliding? " + b.isColliding(c));
